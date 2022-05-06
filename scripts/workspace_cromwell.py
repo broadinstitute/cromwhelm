eba49cdfd015b3528393dbba0a58c06979ec717d
Chris Llanwarne <cjllanwarne@gmail.com>
2022-05-06 17:42:59 -0400

    All except cromshell

--- scripts/workspace_cromwell.py
+++ scripts/workspace_cromwell.py
@@ -2,26 +2,123 @@ import os
 import requests
 import subprocess
 import sys
+import time
 import uuid
 
 
-# If an app exists (which implies that it's running), return app_id. Otherwise None.
+# If a CROMWELL app exists return (app_name, status), otherwise (None, None).
 def check_for_app(env):
-    list_apps_url = f'{env["leonardo_url"]}/api/google/v1/apps'
+    list_apps_url = f'{env["leonardo_url"]}/api/google/v1/apps/{env["google_project"]}'
     r = requests.get(
         list_apps_url,
+        params={
+          'includeDeleted': 'false'
+        },
         headers = {
             'Authorization': f'Bearer {env["token"]}',
             'Referer': 'https://bvdp-saturn-dev.appspot.com/'
         }
     )
     r.raise_for_status()
-    print(r.json())
+
+    for potential_app in r.json():
+        if potential_app['appType'] == 'CROMWELL':
+            potential_app_name = potential_app['appName']
+            potential_app_status = potential_app['status']
+
+            # We found a CROMWELL app in the correct google project and owned by the user. Now just check the workspace:
+            _, workspace_namespace, workspace_name, _ = get_app_details(env, potential_app_name)
+            if workspace_namespace == env['workspace_namespace'] and workspace_name == env['workspace_name']:
+                return potential_app_name, potential_app_status
+
+    return None, None
 
 
-# If a Cromwell PD exists, return pd_id. Otherwise None.
+# Returns (status, workspace_namespace, workspace_name, proxyUrls) for a running app, (None, None, None, None) for a missing/shutdown one:
+def get_app_details(env, app_name):
+    get_app_url = f'{env["leonardo_url"]}/api/google/v1/apps/{env["google_project"]}/{app_name}'
+    r = requests.get(
+        get_app_url,
+        params={
+            'includeDeleted': 'true'
+        },
+        headers={
+            'Authorization': f'Bearer {env["token"]}',
+            'Referer': 'https://bvdp-saturn-dev.appspot.com/'
+        }
+    )
+    if r.status_code == 404:
+        return None, None, None, None
+    else:
+        r.raise_for_status()
+    result_json = r.json()
+    custom_environment_variables = result_json['customEnvironmentVariables']
+    return result_json['status'], custom_environment_variables['WORKSPACE_NAMESPACE'], custom_environment_variables['WORKSPACE_NAME'], result_json.get('proxyUrls')
+
+
+# If a Cromwell PD exists, return pd_name. Otherwise None.
 def check_for_pd(env):
-    raise Exception('TODO')
+    list_apps_url = f'{env["leonardo_url"]}/api/google/v1/disks/{env["google_project"]}'
+    r = requests.get(
+        list_apps_url,
+        params={
+            'saturnApplication': 'CROMWELL',
+            'includeDeleted': 'false'
+        },
+        headers={
+            'Authorization': f'Bearer {env["token"]}',
+            'Referer': 'https://bvdp-saturn-dev.appspot.com/'
+        }
+    )
+    r.raise_for_status()
+
+    for potential_disk in r.json():
+        return potential_disk['name']
+
+    return None
+
+
+# Submits the request to create an app (note: does not block while creation completes)
+def create_app(env, app_name, pd_name, create_pd):
+    create_app_url = f'{env["leonardo_url"]}/api/google/v1/apps/{env["google_project"]}/{app_name}'
+
+    if create_pd:
+        disk_config = {
+            'name': pd_name,
+            'size': 500,
+            'labels': {
+                'saturnApplication': 'CROMWELL',
+                'saturnWorkspaceNamespace': env['workspace_namespace'],
+                'saturnWorkspaceName': env['workspace_name']
+            }
+        }
+    else:
+        disk_config = {
+            'name': pd_name
+        }
+
+    r = requests.post(
+        create_app_url,
+        json = {
+            'labels': {
+                'saturnWorkspaceNamespace': env['workspace_namespace'],
+                'saturnWorkspaceName': env['workspace_name']
+            },
+            'diskConfig': disk_config,
+            "customEnvironmentVariables": {
+                'WORKSPACE_NAME': env['workspace_name'],
+                'WORKSPACE_NAMESPACE': env['workspace_namespace'],
+                'WORKSPACE_BUCKET': env['workspace_bucket'],
+                'GOOGLE_PROJECT': env['google_project'],
+            },
+            'appType': 'CROMWELL'
+        },
+        headers = {
+            'Authorization': f'Bearer {env["token"]}',
+            'Referer': 'https://bvdp-saturn-dev.appspot.com/'
+        }
+    )
+    r.raise_for_status()
 
 
 # If Cromshell is installed True, else False
@@ -29,43 +126,77 @@ def check_for_cromshell():
     raise Exception('TODO')
 
 
-def create_app(env, pd_id):
+def configure_cromshell(env, app_name):
     raise Exception('TODO')
 
 
-def get_app_details(env, app_id):
-    raise Exception('TODO')
-
-
-def configure_cromshell(env, app_id):
-    raise Exception('TODO')
-
-
-def delete_app(env, app_id, delete_pd):
-    raise Exception('TODO')
+# Submits the request to delete an app (note: does not block while deletion completes)
+def delete_app(env, app_name, delete_pd):
+    delete_app_url = f'{env["leonardo_url"]}/api/google/v1/apps/{env["google_project"]}/{app_name}'
+    r = requests.delete(
+        delete_app_url,
+        params={
+            # Do the boolean-to-string manually to make sure it's lowercased correctly:
+            'deleteDisk': 'true' if delete_pd else 'false'
+        },
+        headers={
+            'Authorization': f'Bearer {env["token"]}',
+            'Referer': 'https://bvdp-saturn-dev.appspot.com/'
+        }
+    )
+    r.raise_for_status()
 
 
 def start_command(env):
-    app = check_for_app(env)
+    app_name, app_status = check_for_app(env)
 
-    # if app is None:
-    #     # Note about uuid functions: 'uuid4()' means we get the right format AND it's randomly generated
-    #     app_id = uuid.uuid4()
-    #     pd_id = check_for_pd(env)
-    #     if pd_id is None:
-    #         pd_id = uuid.uuid4()
-    #     create_app(app_id, pd_id)
-    #
-    #     app_details = None
-    #     app_status = 'PROVISIONING'
-    #     while app_status == 'PROVISIONING':
-    #         app_details = get_app_details(env, app_id)
-    #         app_status = app_details['status']
-    #
-    #     if app_status == 'RUNNING':
-    #         configure_cromshell(env, app_id)
-    #     else:
-    #         raise Error(f'Unexpected app state {app_status} for app {app_id}. If the app is left behind, try running "{sys.argv[0]} delete"')
+    print(f'app_name={app_name}; app_status={app_status}')
+
+    if app_name is None:
+        # Note about uuid functions: 'uuid4()' means we get the right format AND it's randomly generated
+        app_name = f'terra-app-{uuid.uuid4()}'
+        print(f'CROMWELL app does not exist. Creating new app with name {app_name}')
+        pd_name = check_for_pd(env)
+        if pd_name is None:
+            create_pd = True
+            pd_name = f'saturn-pd-{uuid.uuid4()}'
+            print(f'CROMWELL disk not found. Creating new disk with name {pd_name}')
+        else:
+            create_pd = False
+            print(f'Existing CROMWELL disk found with name {pd_name}. Reconnecting.')
+        create_app(env, app_name, pd_name, create_pd)
+
+        app_status = 'PROVISIONING'
+        proxy_urls = {}
+        print('Waiting for Cromwell app to come up...')
+        while app_status == 'PROVISIONING':
+            time.sleep(10)
+            app_status, _, _, proxy_urls = get_app_details(env, app_name)
+            print(f'Current {app_name} status: {app_status}...')
+
+        if app_status == 'RUNNING':
+            print(f'Cromwell running with URLs: {proxy_urls}')
+            configure_cromshell(env, app_name)
+        else:
+            raise Exception(f'Unexpected app state {app_status} for app {app_name}. If the app is left behind, try running "{sys.argv[0]} delete"')
+    else:
+        print(f'Cannot continue. Existing CROMWELL app found (app_name={app_name}; app_status={app_status}).')
+        exit(1)
+
+
+def status_command(env):
+    print(f'Scanning from CROMWELL apps and disks for {env["user_email"]} in {env["workspace_namespace"]}/{env["workspace_name"]}')
+
+    app_name, status = check_for_app(env)
+    pd_name = check_for_pd(env)
+
+    if app_name is None:
+        if pd_name is None:
+            print('No CROMWELL app detected. No persistent state detected.')
+        else:
+            print(f'CROMWELL app stopped. Persistent disk {pd_name} is still available.')
+    else:
+        print(f'CROMWELL app detected with name {app_name} and persistent disk {pd_name}.')
 
 
 def stop_command(env):
@@ -76,11 +207,32 @@ def stop_command(env):
 
 
 def delete_command(env):
-    app = check_for_app(env)
+    app_name, app_status = check_for_app(env)
 
-    if app is not None:
-        delete_app(env, app, True)
+    print(f'app_name={app_name}; app_status={app_status}')
+    pd_name = check_for_pd(env)
 
+    if app_name is None:
+        if pd_name is None:
+            print(f'Cannot continue. Existing CROMWELL app not found.')
+            exit(1)
+        else:
+            print('CROMWELL app stopped. Deleting persistent disk...')
+            raise Exception('TODO')
+    else:
+        delete_app(env, app_name, delete_pd=True)
+
+        app_status = 'DELETING'
+        print('Waiting for Cromwell app to be deleted...')
+        while app_status == 'DELETING':
+            time.sleep(10)
+            app_status, _, _, _ = get_app_details(env, app_name)
+            print(f'Current {app_name} status: {app_status}...')
+
+        if app_status == 'DELETED':
+            print(f'CROMWELL app {app_name} and its persistent disk have been deleted successfully.')
+        else:
+            raise Exception(f'Unexpected app state {app_status} for app {app_name}.')
 
 
 def main():
@@ -102,14 +254,13 @@ def main():
         env['referer'] = 'https://app.terra.bio/'
 
     # Fetch the token:
-    token_fetch_command = subprocess.Popen(['gcloud', 'auth', 'print-access-token', env['user_email']])
-    if subprocess.returncode == 0:
-        env['token'], _ = subprocess.communicate()
-    else:
-        raise Exception(f'Bad token fetch: {subprocess.communicate()}')
+    token_fetch_command = subprocess.run(['gcloud', 'auth', 'print-access-token', env['user_email']], capture_output=True, check=True, encoding='utf-8')
+    env['token'] = str.strip(token_fetch_command.stdout)
 
     if sys.argv[1] == 'start':
         start_command(env)
+    elif sys.argv[1] == 'status':
+        status_command(env)
     elif sys.argv[1] == 'stop':
         stop_command(env)
     elif sys.argv[1] == 'delete':
@@ -118,5 +269,5 @@ def main():
         raise Exception(f'Unknown command {sys.argv[1]}')
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     main()
